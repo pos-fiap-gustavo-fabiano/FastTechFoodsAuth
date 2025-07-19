19fd18056457d94d8785a7b263ce414892a461a7
Fabiano Teixeira <fabianotxss@gmail.com>
2025-07-19 19:13:30 -0300

    ajuste response token

--- src/FastTechFoodsAuth.Application/DTOs/AuthResultDto.cs
+++ src/FastTechFoodsAuth.Application/DTOs/AuthResultDto.cs
@@ -1,7 +1,11 @@
-﻿namespace FastTechFoodsAuth.Application.DTOs
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FastTechFoodsAuth.Application.DTOs
 {
     public class AuthResultDto
     {
+        [JsonPropertyName("access_token")]
         public string Access_Token { get; set; }
         public string RefreshToken { get; set; }
         public UserDto User { get; set; }
